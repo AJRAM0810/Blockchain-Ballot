77c8f4e3f1160ebc35a9d0b6c7dbf88b7a624d3f
A.J.RAM <96466685+AJRAM0810@users.noreply.github.com>
2023-04-08 17:47:04 +0530

    Add files via upload

---
+++ backend/data/data.js
@@ -0,0 +1,8 @@
+exports.emails = [
+   {
+      email: 'admin@gmail.com',
+      name: 'Election Officer',
+      role: 'admin',
+      eAddress: '0xC917667764808576FAeef03fF4eF3C64f6eBB499',
+   },
+];
